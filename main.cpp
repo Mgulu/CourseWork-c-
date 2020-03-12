2dccd917940371788754665081166faa5bac3266
gulu.m <mbongenigulu2009@yahoo.co.uk>
2020-03-12 23:49:52 +0000

    Added comments

--- main.cpp
+++ main.cpp
@@ -6,21 +6,27 @@
 
 using namespace std;
 
+/* Prints all the elements in a  vector*/
+void print(vector<int> printList){
+  for(int i =0;i < printList.size();i++){
+    cout << printList[i] << " ";
+  }
+  cout<< endl;
+}
 
 
-
-
-// node and next node
-void addEdge(vector<int> adj[], int u, int v) 
+/* Adds values to the array of vectors */
+void addEdge(vector<int> adj[], int x, int y) 
 {   
-    // Adds to the list in index U
-    adj[u].push_back(v); 
-    // Adds to the list in index v
-    adj[v].push_back(u); 
+    // Adds to the list in index x
+    adj[x].push_back(y); 
+    // Adds to the list in index y
+    adj[y].push_back(x); 
 } 
   
-/*
-void makeMatrix(vector<int> dj[], int N){
+
+/* Makes the graph with all the connections between nodes */
+void makeGraph(vector<int> dj[], int N){
   int listLength = N*N;
   for(int i = 0; i < listLength ;i++){
       if(i+N <listLength){
@@ -32,34 +38,8 @@ void makeMatrix(vector<int> dj[], int N){
     }
   }
 }
-*/
 
-void makeMatrix(vector<int> dj[], int N){
-  int listLength = N*N;
-  for(int i = 0; i < listLength ;i++){
-      if(i+N <listLength){
-          addEdge(dj,i,i+N);
-        }
-      if((i+1) % N == 0) continue;
-      if(i+1 < listLength){
-        addEdge(dj,i,i+1);      
-    }
-  }
-}
-// A utility function to print the adjacency list 
-// representation of graph 
-void adjacent(vector<int> adj[], int V) 
-{ 
-    for (int v = 0; v < V; v++) 
-    { 
-        cout << "\n Adjacency list of vertex "
-             << v << "\n head "; 
-        // auto automatically gets the type of 
-        for (auto x : adj[v]) 
-           cout << "-> " << x; 
-    } 
-} 
-
+/* Prints the elements in the list in grid format */
 void printGraph(int elementsList[],int lengthList,int rows){
   for(int i = 0;i <lengthList;i++){
       cout << elementsList[i];
@@ -69,6 +49,7 @@ void printGraph(int elementsList[],int lengthList,int rows){
     }
 }
 
+/* Opens and reads all the data from a file */
 void openFile(string fileName, int elements[]){
   ifstream ufile(fileName);
   int data;
@@ -76,9 +57,7 @@ void openFile(string fileName, int elements[]){
   ufile >> data; 
   // N x N 
   int N = data;
-  
   cout << data << endl;
-  cout <<"START OF LOOP \n";
   // Gets all the int's inside the txt
   for(int i = 0; i < N*N;i++){
     ufile >> data; 
@@ -87,6 +66,7 @@ void openFile(string fileName, int elements[]){
 }
 
 
+/* Checks if an element is in a vector list */
 int vContains(int num , vector<int> numbers){
   if (std::find(numbers.begin(), numbers.end(), num) != numbers.end())
     // Contains
@@ -96,25 +76,26 @@ int vContains(int num , vector<int> numbers){
 		return 0;
 }
 
-
+/* Looks for all the adjacent elements that are the same
+    Starting from the pivot.
+    reutrns: elements to be changed */
 vector<int> adj(int cur,vector<int> elChange,vector<int> dj[],int elements[],int color){
    for(auto x:dj[cur]){
      // Checks to see if the value exists inside elChange
      bool contained = false;
      if(std::find(elChange.begin(), elChange.end(), x) != elChange.end()) {
       contained = true;
-      }
-     //cout << " Current x :" << x << " Value is "
-     //<< elements[x]  << " Current in DJ :" << cur << endl;
+        }
+     // If the elements are not the same only push back the parent
      if(elements[x] != elements[cur]){
-       //cout << " THIS IS inCORRECT x :" << x << endl;
        elChange.push_back(cur);       
+       // recursively calls adj for elements who have similar values
      } else if(elements[x] == elements[cur] && x > cur && contained == false){
-       //cout << " //////THIS IS correct x :" << x << endl;
+
        elChange.push_back(x); 
        elChange = adj(x,elChange,dj,elements,color);      
      } else if (elements[x] == elements[cur] && x < cur && contained == false){
-       //cout << " //////THIS IS correct x :" << x << endl;
+  
        elChange.push_back(x); 
        elChange = adj(x,elChange,dj,elements,color);  
      }
@@ -122,15 +103,28 @@ vector<int> adj(int cur,vector<int> elChange,vector<int> dj[],int elements[],int
    return elChange ;
 }
 
+/* Procedure that changes the the color of elements in grid */
+void changeColor(vector<int> dj[], int elements[],int color){
+  vector<int> elementsToChange;
+  int prevColor = elements[0];
+  elementsToChange = adj(0,elementsToChange,dj,elements,prevColor);
+  for (int i =0 ; i < elementsToChange.size();i++){
+    elements[elementsToChange[i]] = color ;
+  }  
+  elements[0] = color;
+}
 
+
+/* Checks if two elements are the same then returns a value */
 int gValue(int elements[],int current , int adj){
   if (elements[current] == elements[adj]){
     return 0;
-  } else if (elements[current] != elements[adj]){
+  } else {
     return 1;
   }
 }
 
+/* Checks if the element is next to equal elements */
 int hValue(int elements[],vector<int> dj[],int adj){
     int counter = 0;
     for(auto x:dj[adj]){      
@@ -144,36 +138,32 @@ int hValue(int elements[],vector<int> dj[],int adj){
 }
 
 
-void pathFind(vector<int> dj[],int elements[]){
+/* Checks if an element is in a vector list */
+vector<int> pathFind(vector<int> dj[],int elements[]){
   vector<int> openSet;
   vector<int> closedSet;
   vector<int> path;
+  vector<int> finalPath;
   openSet.push_back(0);
-  int previousFcost = 1000;
+  int previousFcost = 1000;  
   while (openSet.size() > 0){
-     //cout << "First element in openSet " << openSet[0] << endl;
+    
     int currentNode = openSet[0];
-    cout << " This is current Node :" << currentNode << endl;
-    // if statement for the first index
-    //cout << "current Node = " << currentNode << endl;
-    if(currentNode != 0){
-    // calculate adjacent
+   
         for(auto x: openSet){
-          //cout << " In open : " << x << endl;
-          //cout << " Current Node : and X :"
-          //<< currentNode << " " << x << endl;;
+         
           int fCost = hValue(elements,dj,x) + gValue(elements,currentNode,x);
-          cout << " Fcost : " << fCost<<" X Node : "
-          << x <<endl;
-          cout << "Previous F cost "<< previousFcost << endl;         
-          if(fCost < previousFcost && vContains(x, closedSet) == 0 ){
+          //cout << " Fcost : " << fCost<<" X Node : "
+          //<< x <<endl;       
+          if(fCost < previousFcost ){
             currentNode = x;
             previousFcost = fCost;
           }
-        }        
-    } 
+        }  
     
+   
     path.push_back(currentNode);
+
     
     openSet.erase(openSet.begin()+0);
     closedSet.push_back(currentNode);
@@ -181,46 +171,48 @@ void pathFind(vector<int> dj[],int elements[]){
     
     for(auto x: dj[currentNode]){
       if(vContains(x, closedSet) == 1)continue;
-      //if(vContains(x,openSet)==0)
+      if(vContains(x,openSet)==1)continue;
       openSet.push_back(x); 
     }   
 
+    
+    for(auto x: openSet){     
+        if(vContains(x, closedSet) == 1)continue;
+        if(vContains(x,openSet)==1)continue;
+        openSet.push_back(x); 
+      }
+    
+   
+
+
 
 
   }
 
+  
   int steps =0;
   for(int i=0;i < path.size()-1;i++){
-    //cout << path[i] << " -> " << path[i+1] << endl;
-    //cout << path[i] << " -> " << path[i+1] << endl;
-    if(gValue(elements,path[i],path[i+1]) == 1){
+    
+    if(gValue(elements,path[i],path[i+1]) == 1 ){
+     
+      finalPath.push_back(elements[path[i+1]]);
+      changeColor(dj, elements,elements[path[i+1]]);
       steps += gValue(elements,path[i],path[i+1]);
-      cout << path[i] << " -> " << path[i+1] << endl;
-      cout << " Change color to:" << elements[path[i+1]] << endl;
+      
     }
   }
-  // Needs to be divided by 2 as it repeats the first half again
-  cout << " THESE ARE THE STEPS " << steps << endl;
-}
-
-
-
-
-void changeColor(vector<int> dj[], int elements[],int color){
-  vector<int> elementsToChange;
-  int prevColor = elements[0];
-  elementsToChange = adj(0,elementsToChange,dj,elements,prevColor);
-  for (int i =0 ; i < elementsToChange.size();i++){
-    //cout << " THIS IS " <<elementsToChange[i] << endl;
-    elements[elementsToChange[i]] = color ;
-  }  
-  elements[0] = color;
+  
+  return finalPath;
 }
 
 
 
 
 
+
+
+
+/* Checks if all the elements are the same */
 int checkWinner(int elements[],int N){
   int counter = 0;
   int color = elements[0];
@@ -237,120 +229,69 @@ int checkWinner(int elements[],int N){
   }
 }
 
-/*
-void changeColor(vector<int> dj[], int elements[],int N,int color){
-  // first element is count rest are potential elements
-  bool flag = true;
-  int cur[N*N];
-  cur[0] = 1;
-  cur[1] = 0;
-  int count;
-  int changeC = 1;
-  int elementsCh[N*N];
-  elementsCh[0] = 0;
-  int adjac[N*N];
-  int loopLength = cur[0];
-  while (flag == true){
-    flag = false;
-    int loopLength = cur[0]; // length of current
-    // adjacent nodes
-    cout << " THIS IS START \n \n";
-    cout << " Current length of cur  : " << loopLength << endl;///D
-    for(int i = 0; i < loopLength;i++){
-      cout << " Current value = " << cur[i+1] << endl;
-      int adjLength = dj[cur[i+1]].size();
-      
-      adjac[0] = adjLength;
-      cout << " Numve of adj elements : " << adjLength << endl; //D
-      count = 0;
-      for(auto x:dj[cur[i+1]]){ // for each adjacent element
-        cout << " Current adjacent vector : " << x << endl; //D
-        cout << " ////elementsCh[changeC] value is " << elementsCh[changeC-1] << endl;
-        cout << " Elements[x]: " << elements[x] << 
-        " And elements[cur[i]] "<< elements[cur[i+1]]  <<endl; //D
-        if(elements[x] == elements[cur[i+1]] && x > cur[i+1] && elementsCh[changeC-1] != x ){ // if same as current
-          cout << ">>>> change C value before " << elementsCh[0] <<
-          endl;
-          elementsCh[0] = changeC;
-          elementsCh[changeC] = x;
-          changeC++;
-          cout << "***** Value of current in If statement " << x <<
-          endl;
-          cout << " ////elementsCh[changeC] value is " << elementsCh[changeC-1] << endl;
-          cout << " Count = " << count << endl;
-          //elements[x] = color; 
-          flag = true;
-          adjac[count + 1] = x;
-          cout << " Value at adjac[count] : " << adjac[count + 1] << endl;
-          count++;
-        }1
+/* Makes a random grid  */
+void generateGame(int listSize, int elementList[]){
+  for (int i =0; i < listSize; i++){
+          int ran = rand() % 6 + 1;
+          elementList[i] = ran;
+          //cout << gen[i] << endl;
+          }
 
-        
-      }      
-    }    
-
-    cur[0] = count;
-    cout << " 3Count = " << count << endl; 
-    cout << "THIS length of elementsCh[0] "<< elementsCh[0] << endl;
-    cout << "Amount of adjacent Vectors "<< cur[0] << endl;
-    for(int i = 0; i < cur[0] ; i++){
-      cur[i+1] = adjac[i +1] ;
-      cout << " This is cur = " << cur[i+1] << " This is adjac "
-      << adjac[i + 1] << endl;
-    }
-
-  }
-  for(int i = 0; i < elementsCh[0];i++){
-    cout << " Value being changed :" << elementsCh[i+1] << endl;
-    elements[elementsCh[i+1]] = color;
-  }
-  
-  elements[0] = color;
 }
-*/
 
 
 int main() { 
-  string fileNam = "example.txt";
-  /*  Making list of elements     */
-  //opens file
-  ifstream ufile(fileNam);
-  int data;
-  // gets first element of list
-  ufile >> data; 
-  int rows = data;
-  ufile.close();
-  int elementList[rows*rows];
-  // returns list of elements
-  openFile(fileNam,elementList);
+  char choice;
+  cout << "WELCOME , file or generate grid ? F/G" << endl;
+  cin >> choice;
+  int rows;
+  int listLength;
+  string fileNam;
+  if(choice == 'F'){    
+    cout << "Enter file name : ";
+    cin >> fileNam;
+    ifstream ufile(fileNam);
+    ufile >> rows ;
+    listLength = rows*rows;
+    ufile.close();
+    
+
+  } else if ( choice == 'G'){
+    cout << "Enter how many rows? : ";
+     cin >> rows ;
+    listLength = rows*rows;
+    
+
+  }
+
+  int elementList[listLength];
+  int tempList[listLength];
+
+  if(choice == 'F'){
+    openFile(fileNam,elementList);
+  } else if(choice == 'G'){
+    generateGame(listLength, elementList);
+  }
+  
+  
   /*   Making graph       */
-  int listLength = rows*rows;
+  
   vector<int> matr[listLength];
+  vector<int> tempMatr[listLength];
+  makeGraph(tempMatr,rows);
+  makeGraph(matr,rows);
 
-  makeMatrix(matr,rows);
 
-  //adjacent(matr,listLength);
   cout << endl;
 
+  
+  for(int i=0; i<listLength; ++i)
+        tempList[i] = elementList[i];
+  
+  
   printGraph(elementList,listLength,rows);
-
   
-
-
-  /*
-  for(auto x: matr[0]){
-    cout << x << endl;
-  } */
-
-  ///cout << "Lenght of matr[0] = " << matr[0].size() << endl;
-  /*    change color
-             */
-  cout << "Start of chnage color " << endl;
-  
-   
-
-
-  pathFind(matr,elementList);
+  vector<int> bestPath = pathFind(tempMatr,tempList);
 
   int finished = 0;
   while (finished != 1){
@@ -360,8 +301,15 @@ int main() {
     cin >> color;
     changeColor(matr, elementList,color);
     printGraph(elementList,listLength,rows);
+    if(color == 0){
+      finished = 1;
+    } else{
     finished = checkWinner(elementList,listLength);
+    }
   }
+  cout << " Minimum moves to finish was " << bestPath.size()<<endl;
+  cout<< "These were the best moves :";
+  print(bestPath);
 
 
 
