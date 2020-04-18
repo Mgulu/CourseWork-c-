ba2899eb2199b3b7611a509fc3c42d43b0a71f15
gulu.m <mbongenigulu2009@yahoo.co.uk>
2020-04-18 14:23:51 +0000

    Algorithm has been made, needs to be optimised more

--- main.cpp
+++ main.cpp
@@ -6,6 +6,27 @@
 
 using namespace std;
 
+class Node {       
+  public:                
+    int gValue;
+    int hvalue;
+    int fvalue;     
+    int *grid;  
+    Node(int gValeur, int hValeur,int fValeur,int elements[]) {
+      gValue = gValeur;
+      hvalue = hValeur;
+      fvalue = fValeur;
+      grid = elements;
+      
+    }
+};
+
+void deepCopy(int newElements[],int elements[],int elementsLength){
+  for(int i=0; i<elementsLength; ++i)
+    newElements[i] = elements[i];
+
+}
+
 /* Prints all the elements in a  vector*/
 void print(vector<int> printList){
   for(int i =0;i < printList.size();i++){
@@ -149,13 +170,214 @@ int *goalState(int adj, int elements[]){
 
 int hValue(int elements[],vector<int> dj,int adj){
 
-  changeColor(dj, elements,elements[path[i+1]])
+  changeColor(dj, elements,elements[path[i+1]] )
   
 }
 */
 
+/* Checks if all the elements are the same */
+int checkWinner(int elements[],int N){
+  int counter = 0;
+  int color = elements[0];
+  for(int i =1;i < N ; i++){
+    if(elements[i] == color){
+      counter +=1;
+    }
+  }
+  if(counter == N-1){
+    cout << "\n **" <<"Well Done , You've won ! **";
+    return 1;
+  }else{
+    return 0;
+  }
+}
 
-/* Checks if an element is in a vector list */
+/* Checks if all the elements are the same */
+int missPlaced(int elements[],int N){
+  int counter = 0;
+  int color = elements[0];
+  for(int i =1;i < N ; i++){
+    if(elements[i] != color){
+      counter +=1;
+    }
+  }
+  return counter;
+}
+
+/*
+int missPlaced(int elements[],int N){
+  //int counter = 0;
+  //int color = elements[0];
+  std::unordered_set<int> colours;
+  for(int i =0;i < N ; i++){
+    if (colours.find(elements[i]) == colours.end()){
+        colours.insert(elements[i]);
+      } 
+  }
+  unordered_set<int> :: iterator itr;  
+    // iterator itr loops from begin() till end() 
+  for (itr = colours.begin(); itr != colours.end(); itr++) {
+      Fcout << *itr << " " << endl; 
+    } 
+  
+  return colours.size();
+}*/
+
+
+
+bool isSame(int elements[],int nElements[],int elementsSize){
+  for(int i=0;i < elementsSize;i ++){
+    if(elements[i] != nElements[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+int won(int elements[],int size){
+  int same[2] = {0,0};
+  int u = 0; 
+  for(int i =0;i < size ; i++){
+    if(elements[0] != elements[i]){
+      same[u] = elements[i];
+      u += 1;
+    }
+  }
+  if(same[0] != same[1]){
+    return 1;
+  } else if (same[0] == same[1]){
+    return 0;
+  }
+
+}
+
+
+void pathFind(vector<int> dj[],int elements[],int listSize,int rowSize){
+  vector<Node> openSet;
+  vector<Node> closedSet;
+  vector<int> path;
+  Node rootNode(0,0,0,elements);
+  openSet.push_back(rootNode); 
+  Node currentNode = openSet[0];
+  int loop = 0;
+  int currentUnique = missPlaced(elements,listSize);
+  int previousGrid[listSize];
+  while (openSet.size() > 0 ){
+    int lowestFcost = 100000;
+    int index = 0;
+    int counter = 0;
+    for(auto x: openSet){
+      if(x.fvalue < lowestFcost){
+        lowestFcost = x.fvalue;
+        currentNode = x;
+        index = counter;   
+      }
+      counter += 1;
+    }
+    
+   
+
+    loop +=1;
+    closedSet.push_back(currentNode);
+    //cout << " ClosedSet Size : " << closedSet.size() << endl;
+    cout << " LOOP :" << loop << endl;
+   // cout << " LowestFcost & CurrentGrid :" << lowestFcost << endl;  
+    printGraph(currentNode.grid,listSize,rowSize);
+    cout << endl;
+    
+    cout << endl;
+    cout << "Current Gvalue " << currentNode.gValue << endl;
+    cout << "Current Hvalue " << currentNode.hvalue << endl;
+    if(missPlaced(currentNode.grid,listSize) < 3){
+      int output = currentNode.gValue + won(currentNode.grid,listSize);
+      cout << "LOWEST AMOUNT OF MOVES :" << output  << endl;
+      cout << "WINNER WINNER CHICKEN DINNER " << endl;
+      exit(0);      
+    } 
+
+    
+
+    for(int i =1;i <10;i++){  
+      int * currentGrid;
+      currentGrid = new int [listSize];    
+      //int currentGrid[listSize];
+      //cout << currentGrid << endl;
+      deepCopy(currentGrid,currentNode.grid,listSize);       
+      //cout << endl;
+      changeColor(dj, currentGrid,i);
+      //cout << endl;
+      
+
+
+      bool openFlag = false;
+      for(auto x: openSet){
+        if(isSame(x.grid,currentGrid,listSize) == true){
+          openFlag = true;
+          continue;
+        }
+      }
+      
+      bool closedFlag = false;
+      for(auto x:closedSet){
+        if(isSame(currentGrid,x.grid,listSize) == true &&
+          x.gValue > currentNode.gValue +1){
+            x.gValue = currentNode.gValue + 1;
+            closedFlag = true;
+            //cout << " IN HERE " << endl;
+            //printGraph(currentGrid,listSize,rowSize);
+            //cout << " IN HERE " << endl;
+            continue;
+          }
+        else if(isSame(currentGrid,x.grid,listSize) == true &&
+          x.gValue < currentNode.gValue +1 ){
+            closedFlag = true;
+            //cout << " Outchea " << endl;
+            //printGraph(currentGrid,listSize,rowSize);
+            //cout << " Outchea " << endl;
+            continue;
+          }
+        
+        if (openFlag == false && closedFlag == false){
+          int hValue = missPlaced(currentGrid,listSize);
+          int gValue = currentNode.gValue +1 ;
+          int fValue = hValue + gValue;
+          Node childNode(gValue,hValue,fValue,currentGrid);
+          openSet.push_back(childNode);   
+          //cout << " Added To Openset " << endl;         
+          //printGraph(currentGrid,listSize,rowSize);  
+          //cout << " hValye " << childNode.hvalue << endl;
+          //cout << " Added To Openset " << endl;
+          }
+
+      }     
+
+    }
+  
+  vector<int> inOpen;
+  int indexN = 0;
+  for(auto x: openSet){
+    if(isSame(currentNode.grid,x.grid,listSize)){
+      inOpen.push_back(indexN);
+    }
+    indexN +=1;
+  }
+
+  for(auto x:inOpen){
+    openSet.erase(openSet.begin() + x);
+  }
+  cout << endl;
+
+  
+  cout << " OpenSet Size : " << openSet.size() << endl;
+  
+  }
+
+
+
+}
+
+
+/* Checks if an element is in a vector list 
 vector<int> pathFind(vector<int> dj[],int elements[]){
   vector<int> openSet;
   vector<int> closedSet;
@@ -198,14 +420,7 @@ vector<int> pathFind(vector<int> dj[],int elements[]){
         if(vContains(x,openSet)==1)continue;
         openSet.push_back(x); 
       }
-    
-   
-
-
-
-
   }
-
   
   int steps =0;
   for(int i=0;i < path.size()-1;i++){
@@ -220,6 +435,7 @@ vector<int> pathFind(vector<int> dj[],int elements[]){
   
   return finalPath;
 }
+*/
 
 
 
@@ -228,22 +444,6 @@ vector<int> pathFind(vector<int> dj[],int elements[]){
 
 
 
-/* Checks if all the elements are the same */
-int checkWinner(int elements[],int N){
-  int counter = 0;
-  int color = elements[0];
-  for(int i =1;i < N ; i++){
-    if(elements[i] == color){
-      counter +=1;
-    }
-  }
-  if(counter == N-1){
-    cout << "\n **" <<"Well Done , You've won ! **";
-    return 1;
-  }else{
-    return 0;
-  }
-}
 
 /* Makes a random grid  */
 void generateGame(int listSize, int elementList[]){
@@ -283,6 +483,9 @@ int main() {
   int elementList[listLength];
   int tempList[listLength];
 
+
+
+  
   if(choice == 'F'){
     openFile(fileNam,elementList);
   } else if(choice == 'G'){
@@ -290,11 +493,17 @@ int main() {
   }
   
   
+  /* deep copy 
+  int deepCopy[listLength];
+  memcpy(deepCopy,elementList,listLength);
+  cout << elementList <<  endl;
+  */
+  
+  
+  
   /*   Making graph       */
   
   vector<int> matr[listLength];
-  vector<int> tempMatr[listLength];
-  makeGraph(tempMatr,rows);
   makeGraph(matr,rows);
 
 
@@ -305,10 +514,12 @@ int main() {
         tempList[i] = elementList[i];
   
   
-  printGraph(elementList,listLength,rows);
+  printGraph(tempList,listLength,rows);  
   
-  vector<int> bestPath = pathFind(tempMatr,tempList);
+  
+  pathFind(matr,tempList,listLength,rows);
 
+  /*
   int finished = 0;
   while (finished != 1){
     int color;
@@ -326,7 +537,7 @@ int main() {
   cout << " Minimum moves to finish was " << bestPath.size()<<endl;
   cout<< "These were the best moves :";
   print(bestPath);
-
+  */
 
 
 
